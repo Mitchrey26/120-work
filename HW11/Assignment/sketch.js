b4535daa72ad46dfbafd3f766d7d6ea5e8a183a0
Mitch <mr119648@umconnect@umt.edu>
2018-11-13 12:49:11 -0700

    Fix

--- HW11/Assignment/sketch.js
+++ HW11/Assignment/sketch.js
@@ -1,27 +1,18 @@
+//trippy party !
+//by Mitch Reynolds
 
-let bubbles = [];
 
+let party = [];
+
+//Sets up the canvas for the snow
 function setup() {
-    createCanvas(windowWidth, 400);
-    let b = new Bubble(width/2, height/2, 10);
-    bubbles.push(b);
+    createCanvas(windowWidth, windowHeight);
+    let b = new Party(width/2, height/2, 10);
+    party.push(b);
 }
 
-function mouseMoved() {
-    let r = random(5, 30);
-    let b = new Bubble(mouseX, mouseY, r);
-    bubbles.push(b);
-}
-
-function draw() {
-    background(0);
-    for (let i = 0; i < bubbles.length; i++) {
-        bubbles[i].move();
-        bubbles[i].show();
-    }
-}
-
-class Bubble {
+//Conffetti class is made known in construuctor
+class Party {
     constructor(x, y, r) {
         this.x = x;
         this.y = y;
@@ -29,21 +20,32 @@ class Bubble {
     }
 
     move() {
-        this.x = this.x + random(-5, 5);
-        this.y = this.y + random(-5, 5);
+        this.x = this.x - 1.5;
+        this.y = this.y - .5;
     }
 
-    pour(){
-        //Updates this.x position by subtracting 0.1 from its current value
-        this.x -= 0.1;
-        //Updates this.y position by adding a random value between 1 and 3 to its current value
-        this.y += random( 1, 3 );
-      }
-      
     show() {
-        stroke(random(255), random(255), random(255));
-        strokeWeight(1);
-        noFill();
-        ellipse(this.x, this.y, this.r * 2);
+        fill(random(255), random(255), random(255), random(255));
+        ellipse(this.x, this.y, this.r * 1.5);
+        stroke(random(255), random(255), random(255), random(255));
+        strokeWeight(4);
+        frameRate(10);
+    }
+}
+
+//Guves us an instant party wherever we want!
+function mouseDragged() {
+    let r = random(10, 20);
+    let b = new Party(mouseX, mouseY, r);
+    party.push(b);
+
+}
+
+//Draws strobing orbs
+function draw() {
+    background('black');
+    for (let i = 0; i < party.length; i++) {
+        party[i].move();
+        party[i].show();
     }
 }
